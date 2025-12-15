d548346ab037bbc5299a2bd54a686cb55a592c1b
Kamal Pandey <cdsp425@gmail.com>
2025-12-15 23:58:21 +0530

    First Commit

---
+++ src/main/java/com/sample/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,24 @@
+package com.sample.springboot.myfirstwebapp.hello;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello! What is you doing today?";
+	}
+	
+	
+	///src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+	@RequestMapping("say-hello-jsp")
+	
+	public String sayHelloJsp() {
+		return "sayHello";
+	}
+}
---
+++ src/main/java/com/sample/springboot/myfirstwebapp/todo/Todo.java
@@ -0,0 +1,81 @@
+package com.sample.springboot.myfirstwebapp.todo;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Todo {
+	
+	public Todo() {
+		
+	}
+	@Size(min = 10, message = "Enter atleast 10 characters")
+	private String description;
+	private LocalDate targetDate;
+	private boolean done;
+
+	public Todo(int id, String userName, String description, LocalDate targetDate, boolean done) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.description = description;
+		this.targetDate = targetDate;
+		this.done = done;
+	}
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String username;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return username;
+	}
+
+	public void setUserName(String username) {
+		this.username = username;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getTargetDate() {
+		return targetDate;
+	}
+
+	public void setTargetDate(LocalDate targetDate) {
+		this.targetDate = targetDate;
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", userName=" + username + ", description=" + description + ", targetDate="
+				+ targetDate + ", done=" + done + "]";
+	}
+
+}
